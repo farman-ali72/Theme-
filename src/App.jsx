8746ae7c24266db355cc4e654a8cca5298e0ca81
farman-ali72 <codingwithfa.72@gmail.com>
2025-05-20 19:18:39 +0500

    farman code open

--- src/App.jsx
+++ src/App.jsx
@@ -77,87 +77,87 @@
 
 
 
-// import React from 'react'
-// import { useState } from 'react'
-// const App = () => {
-// const [theme, setTheme] = useState("dark")
-// function handle(){
-//   setTheme(theme==="dark" ? "light" : "dark")
-// }
-//   return (
-//     <div className={`${theme==="dark"? "bg-black text-white" : "bg-white text-black"} h-screen flex justify-center items-center gap-10 flex-col`}>
-//       <h1 className='border-2 black p-5 rounded-3xl text-3xl font-bold uppercase'>{theme==="dark"? "dark mode on" : "light mode on"}
+import React from 'react'
+import { useState } from 'react'
+const App = () => {
+const [theme, setTheme] = useState("dark")
+function handle(){
+  setTheme(theme==="dark" ? "light" : "dark")
+}
+  return (
+    <div className={`${theme==="dark"? "bg-black text-white" : "bg-white text-black"} h-screen flex justify-center items-center gap-10 flex-col`}>
+      <h1 className='border-2 black p-5 rounded-3xl text-3xl font-bold uppercase'>{theme==="dark"? "dark mode on" : "light mode on"}
      
 
-//       </h1>
-//       <button onClick={handle} className='border-2 black p-5 rounded-3xl text-2xl font-bold uppercase' >click</button>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import React from 'react'
-
-  import { useState } from 'react'
-
-const App = () => {
-  
-const [theme, settheme] = useState("dark")
-
-
-
-  function click(){
-
-    settheme(theme==="dark"? "light": "dark")
-    
-  }
-  
-  return (
-    <div className={`h-[100vh]
-      ${theme=== "dark"? "bg-black text-white":"bg-white text-black"}
-      flex 
-    justify-center items-center flex-col gap-5 border-rounded `}>
-      <h1 className={`text-3xl uppercase rounded-2xl  `}> 
-        change theme 
       </h1>
-
-      <p className='border-1 p-5 rounded-3xl' onClick={click}> submit </p>
+      <button onClick={handle} className='border-2 black p-5 rounded-3xl text-2xl font-bold uppercase' >click</button>
     </div>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+
+//   import { useState } from 'react'
+
+// const App = () => {
+  
+// const [theme, settheme] = useState("dark")
+
+
+
+//   function click(){
+
+//     settheme(theme==="dark"? "light": "dark")
+    
+//   }
+  
+//   return (
+//     <div className={`h-[100vh]
+//       ${theme=== "dark"? "bg-black text-white":"bg-white text-black"}
+//       flex 
+//     justify-center items-center flex-col gap-5 border-rounded `}>
+//       <h1 className={`text-3xl uppercase rounded-2xl  `}> 
+//         change theme 
+//       </h1>
+
+//       <p className='border-1 p-5 rounded-3xl' onClick={click}> submit </p>
+//     </div>
+//   )
+// }
+
+// export default App
 
 
 
